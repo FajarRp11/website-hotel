4a08c2a0a0b24561b63274535e344a3628cf1eaf
FajarRp11 <fajarrahyu1103@gmail.com>
2025-01-12 20:49:58 +0700

    menambah view action room

--- app/Filament/Resources/RoomResource.php
+++ app/Filament/Resources/RoomResource.php
@@ -112,7 +112,9 @@ class RoomResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
