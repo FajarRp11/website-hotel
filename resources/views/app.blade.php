41655779b75bab5925a6f9ecb3e9ded315fe6af4
FajarRp11 <fajarrahyu1103@gmail.com>
2025-01-13 15:00:34 +0700

    section about, favorite room

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,10 @@
 <x-app-layout>
-    <section class="h-screen w-full overflow-hidden lg:p-2 text-gray-100">
-        <div class="h-full w-full bg-hero-pattern bg-cover bg-center lg:rounded-lg overflow-hidden">
+    <section class="h-screen w-full overflow-hidden md:p-2 text-gray-100">
+        <div class="h-full w-full bg-hero-pattern bg-cover bg-center md:rounded-lg overflow-hidden">
             <div class="h-full w-full bg-black/65 px-8 lg:px-16 flex flex-col justify-center items-start">
                 <div class="max-w-md flex flex-col justify-center items-start gap-6">
-                    <div class="flex items-center justify-center font-semibold rounded-full text-sm bg-white p-2 gap-2 text-gray-900">
+                    <div
+                    class="flex items-center justify-center font-semibold rounded-full text-sm bg-white p-2 gap-2 text-gray-900">
                         <img src={{ asset('images/crown.png') }} class="w-5" alt="">
                         <p>ASEAN Green Hotel Award 2024</p>
                     </div>
@@ -11,6 +12,90 @@
                     <p class="text-lg">Enjoy the comfort and beauty we offer, from modern facilities to exceptional service that caters to your every need.</p>
                     <button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-semibold text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Book Now</button>
                 </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="grid grid-cols-2 p-8 text-gray-900">
+        <div class="hidden md:flex justify-center items-center col-span-1">
+            <div class="max-w-lg">
+                <img src="{{ asset('images/1.jpg') }}" class="w-96" alt="hotel">
+            </div>
+        </div>
+        <div class="flex justify-center items-start flex-col col-span-2 md:col-span-1">
+            <div class="max-w-lg">
+                <h1 class="text-3xl font-bold mb-3">About Us</h1>
+                <div class="flex flex-col items-start justify-center gap-3 text-gray-700">
+                    <p>
+                        Welcome to Hotel, where comfort meets elegance. Strategically located in the heart of Bali, our hotel offers a perfect blend of modern amenities and warm hospitality.
+                    </p>
+                    <p>
+                        Our rooms and suites are thoughtfully designed to cater to your comfort, featuring luxurious bedding, high-speed Wi-Fi, and stunning city views. Enjoy our state-of-the-art facilities, including a rooftop pool, fine dining restaurants, and a fully equipped fitness center.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="px-12 py-8 text-gray-900">
+        <h1 class="text-3xl font-bold mb-4 text-center">Our Favorite Room</h1>
+        <div class="flex justify-center items-center gap-4">
+            <div class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <img src="{{ asset('images/1.jpg') }}" class="rounded-md" alt="hotel">
+                <p class="font-semibold my-4 text-lg">Room Number: 1</p>            
+                <h2 class="mb-2 font-semibold text-gray-900 dark:text-white">Facilities:</h2>
+                <ul class="max-w-md space-y-1 text-sm text-gray-500 list-inside dark:text-gray-400">
+                    <li class="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 me-2 text-blue-600 dark:text-blue-400 flex-shrink-0">
+                            <path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                        </svg>
+                        Hot water
+                    </li>
+                    <li class="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 me-2 text-blue-600 dark:text-blue-400 flex-shrink-0">
+                            <path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                        </svg>
+                        Wi-Fi
+                    </li>
+                </ul>
+            </div>
+            <div class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <img src="{{ asset('images/1.jpg') }}" class="rounded-md" alt="hotel">
+                <p class="font-semibold my-4 text-lg">Room Number: 1</p>            
+                <h2 class="mb-2 font-semibold text-gray-900 dark:text-white">Facilities:</h2>
+                <ul class="max-w-md space-y-1 text-sm text-gray-500 list-inside dark:text-gray-400">
+                    <li class="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 me-2 text-blue-600 dark:text-blue-400 flex-shrink-0">
+                            <path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                        </svg>
+                        Hot water
+                    </li>
+                    <li class="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 me-2 text-blue-600 dark:text-blue-400 flex-shrink-0">
+                            <path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                        </svg>
+                        Wi-Fi
+                    </li>
+                </ul>
+            </div>
+            <div class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <img src="{{ asset('images/1.jpg') }}" class="rounded-md" alt="hotel">
+                <p class="font-semibold my-4 text-lg">Room Number: 1</p>            
+                <h2 class="mb-2 font-semibold text-gray-900 dark:text-white">Facilities:</h2>
+                <ul class="max-w-md space-y-1 text-sm text-gray-500 list-inside dark:text-gray-400">
+                    <li class="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 me-2 text-blue-600 dark:text-blue-400 flex-shrink-0">
+                            <path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                        </svg>
+                        Hot water
+                    </li>
+                    <li class="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 me-2 text-blue-600 dark:text-blue-400 flex-shrink-0">
+                            <path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                        </svg>
+                        Wi-Fi
+                    </li>
+                </ul>
             </div>
         </div>
     </section>
